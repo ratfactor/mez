a6e9d3ef7e4300e11d1616bb825a40d1300d1ed4
Dave Gauer <dave@ratfactor.com>
2023-04-29 22:40:54 -0400

    Main header complete

--- mez.zig
+++ mez.zig
@@ -87,4 +87,37 @@ pub fn main() !void {
         buffer[33],
         buffer[32],
     });
+
+    //      42-43 Size of an entry in the program header table
+    std.debug.print("42-43 - Size of program header entries.: {d:0>2}{d:0>2} bytes\n", .{
+        buffer[43],
+        buffer[42],
+    });
+
+    //      44-45 Number of entries in the program header table
+    std.debug.print("44-45 - Number of program entries: {d:0>2}{d:0>2}\n", .{
+        buffer[45],
+        buffer[44],
+    });
+
+	/*
+	program headers
+	 0-3 	Type of segment (see below)
+			0 = null - ignore the entry
+			1 = load - clear p_memsz bytes at p_vaddr to 0
+					   then copy p_filesz bytes
+					   from p_offset to p_vaddr
+			2 = dynamic - requires dynamic linking
+			3 = interp - file path to interpreter
+			4 = note section
+	4-7 	offset in the file, data for this segment (p_offset)
+	8-11 	start to put this segment in virtual memory (p_vaddr)
+	12-15 	Undefined for the System V ABI
+	16-19 	Size of the segment in the file (p_filesz)
+	20-23 	Size of the segment in memory (p_memsz)
+	24-27 	Flags (see below)
+	        1 = executable, 2 = writable, 4 = readable. 
+	28-31 	The alignment for this section (must be a power of 2) 
+	*/
+
 }
