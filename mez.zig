c4dc4e742bcbb107c0d3416c228c1d600cc5782a
Dave Gauer <dave@ratfactor.com>
2023-05-09 15:04:56 -0400

    Print const (de-verbosify), segment ASCII box art

--- mez.zig
+++ mez.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const print = std.debug.print;
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -13,6 +14,7 @@ pub fn main() !void {
 
     try file.reader().readNoEof(buffer);
 
+    // Main Header
     // Adapted from excellent table at https://wiki.osdev.org/ELF
     //
     //   Position Value
@@ -37,58 +39,58 @@ pub fn main() !void {
     //      48-49 Number of entries in the section header table
     //      50-51 Index in section header table with section names
 
-    std.debug.print("ELF HEADER\n", .{});
-    std.debug.print("  0-3 - four bytes of magic: ", .{});
+    print("-----------------------------------------\n", .{});
+    print("Main ELF Header\n", .{});
+    print("  0-3 - four bytes of magic (0x7f,'ELF'): ", .{});
 
     const magic = "\x7fELF";
     for (magic, buffer[0..4]) |should, is| {
-        if (should == is) {
-            std.debug.print("{x}", .{is});
-        } else {
-            std.debug.print("\nOH NO! Got {x} instead of {x}.\n", .{ is, should });
+        if (should != is) {
+            print("\nOH NO! Got {x} instead of {x}.\n", .{ is, should });
             std.os.exit(1);
         }
     }
-    std.debug.print(" GOOD!\n", .{});
+    print("Matched!\n", .{});
 
     // 32 bit?
     if (buffer[4] == 1) {
-        std.debug.print("    4 - 32-bit, as expected.\n", .{});
+        print("    4 - 32-bit, as expected.\n", .{});
     } else {
-        std.debug.print("\nOH NO! Got {x} instead of 1 for 32-bits.\n", .{buffer[4]});
+        print("\nOH NO! Got {x} instead of 1 for 32-bits.\n", .{buffer[4]});
         std.os.exit(1);
     }
 
     // little endian?
     if (buffer[5] == 1) {
-        std.debug.print("    5 - little-endian, as expected.\n", .{});
+        print("    5 - little-endian, as expected.\n", .{});
     } else {
-        std.debug.print("\nOH NO! Got {x} instead of 1 for endianness.\n", .{buffer[5]});
+        print("\nOH NO! Got {x} instead of 1 for endianness.\n", .{buffer[5]});
         std.os.exit(1);
     }
 
     const entry_addr = cast(u32, buffer[24..28]);
-    std.debug.print("24-27 - Program entry addr: 0x{x:0>8}\n", .{entry_addr});
+    print("24-27 - Program entry addr: 0x{x:0>8}\n", .{entry_addr});
 
     const ph_offset = cast(u32, buffer[28..32]);
-    std.debug.print("28-31 - Program header offset (in this file): 0x{x}\n", .{ph_offset});
+    print("28-31 - Program header offset (in this file): 0x{x}\n", .{ph_offset});
 
-    std.debug.print("32-35 - Section header offset (in this file): 0x{x}\n", .{cast(u32, buffer[32..36])});
+    print("32-35 - Section header offset (in this file): 0x{x}\n", .{cast(u32, buffer[32..36])});
 
     //      40-41 Header size
-    std.debug.print("40-41 - Size of this header: {d} bytes\n", .{cast(u16, buffer[40..42])});
+    print("40-41 - Size of this header: {d} bytes\n", .{cast(u16, buffer[40..42])});
 
 
     //      42-43 Size of an entry in the program header table
     const ph_size = cast(u16, buffer[42..44]);
-    std.debug.print("42-43 - Size of program header entries: {d} bytes\n", .{ph_size});
+    print("42-43 - Size of program header entries: {d} bytes\n", .{ph_size});
 
     //      44-45 Number of entries in the program header table
     const ph_count = cast(u16, buffer[44..46]);
-    std.debug.print("44-45 - Number of program entries: {d}\n", .{ph_count});
+    print("44-45 - Number of program entries: {d}\n", .{ph_count});
 
 
-    //	program headers
+    // Program headers
+    // Adapted from excellent table at https://wiki.osdev.org/ELF
     //	 0-3 	Type of segment (see below)
     //			0 = null - ignore the entry
     //			1 = load - clear p_memsz bytes at p_vaddr to 0
@@ -111,38 +113,56 @@ pub fn main() !void {
 
 
     for(0..ph_count) |_|{
-        std.debug.print("------------------------\n", .{});
-        std.debug.print("Program Header @ 0x{x}\n", .{ph_pos});
+        print("-----------------------------------------\n", .{});
+        print("Program Header @ 0x{x}\n", .{ph_pos});
 
         //	 0-3 	Type of segment
         const ph_type = cast(u32, buffer[ph_pos..ph_pos+4]);
 
         if (ph_type == 1)  {
-            std.debug.print("  Segment type: {d} (LOAD, as expected)\n", .{ph_type});
+            print("  Segment type: {d} ('load', as expected)\n", .{ph_type});
         }
     else{
-            std.debug.print("\nERROR: Expected segment type 1 (LOAD), got {d} instead.\n", .{ph_type});
+            print("\nERROR: Expected segment type 1 ('load'), got {d} instead.\n", .{ph_type});
             printMem(buffer, ph_pos);
             std.os.exit(1);
         }
 
         //	4-7 	offset in file
         const seg_foffset = cast(u32, buffer[ph_pos+4..ph_pos+8]);
-        std.debug.print("  File offset: 0x{x}\n", .{seg_foffset});
+        print("  File offset: 0x{x}\n", .{seg_foffset});
 
         //	16-19 	Size of the segment in the file (p_filesz)
         const seg_fsize = cast(u32, buffer[ph_pos+16..ph_pos+20]);
-        std.debug.print("  File size: {d} bytes\n", .{seg_fsize});
+        print("  File size: {d} bytes\n", .{seg_fsize});
 
         //	8-11 	start to put this segment in virtual mem (p_vaddr)
         const seg_moffset = cast(u32, buffer[ph_pos+8..ph_pos+12]);
-        std.debug.print("  Target memory start: 0x{x}\n", .{seg_moffset});
+        print("  Target memory start: 0x{x}\n", .{seg_moffset});
 
         //	20-23 	Size of the segment in memory (p_memsz)
         const seg_msize = cast(u32, buffer[ph_pos+20..ph_pos+24]);
-        std.debug.print("  Target memory size: {d} bytes\n", .{seg_msize});
+        print("  Target memory size: {d} bytes\n", .{seg_msize});
 
-        
+        print(
+            \\  Memory mapping:
+            \\    +--------------------+     +--------------------+
+            \\    | File               | ==> | Memory             |
+            \\    |====================|     |====================|
+            \\    | 0x{x: <7}          |     | 0x{x:0>8}         |
+            \\    |   Load: {d: <7}    |     |   Alloc: {d: <7}   |
+            \\    | 0x{x: <7}          |     | 0x{x:0>8}         |
+            \\    +--------------------+     +--------------------+
+            \\
+        ,
+            .{
+                seg_foffset,
+                seg_moffset,
+                seg_fsize,
+                seg_msize,
+                seg_foffset+seg_fsize,
+                seg_moffset+seg_msize,
+            });
 
         // Move to next header
         ph_pos += ph_size;
@@ -156,17 +176,17 @@ fn printMem(mem: []u8, pos: usize) void {
     const end=std.math.min(mem.len, pos+4);
     for(mem[start..end], start..end)|m,c|{
         if(c==pos){
-            std.debug.print("{x}: {x} <--- pos\n", .{c, m});
+            print("{x}: {x} <--- pos\n", .{c, m});
         }
         else{
-            std.debug.print("{x}: {x}\n", .{c, m});
+            print("{x}: {x}\n", .{c, m});
         }
     }
 }
 
 fn cast(T: anytype, bytes: []u8) T {
     if(@sizeOf(T) != bytes.len){
-            std.debug.print("Mismatch for cast: type is {d} bytes but slice is {d}.\n", .{ @sizeOf(T), bytes.len });
+            print("Mismatch for cast: type is {d} bytes but slice is {d}.\n", .{ @sizeOf(T), bytes.len });
             std.os.exit(1);
     }
     
